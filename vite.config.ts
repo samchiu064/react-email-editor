0ac2afd3f7b36f6ae1fc764842025e137cf330fe
Sam Chiu <sam.csdi.chiu@acer.com>
2025-04-28 11:21:45 +0800

    chore: add gh-pages to the project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-email-editor/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
